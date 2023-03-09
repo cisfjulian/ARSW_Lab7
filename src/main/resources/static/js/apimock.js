ab9c442ad9427b9e8be14b6ad2ad30398f5145ec
unknown <julian.largo@labinfo.is.escuelaing.edu.co>
2023-03-09 09:28:51 -0500

    avances clase

---
+++ src/main/resources/static/js/apimock.js
@@ -0,0 +1,38 @@
+//@author hcadavid
+
+apimock=(function(){
+
+	var mockdata=[];
+
+	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
+	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
+	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
+	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+    mockdata["Test"] = [{author: "Test", "points":[{"x":137,"y":82},{"x":123,"y":161}], "name": "house4"},
+            	{author: "Test", "points":[{"x":75,"y":12},{"x":210,"y":167}], "name": "gear4"}];
+
+	return {
+		getBlueprintsByAuthor:function(authname,callback){
+			callback(
+				mockdata[authname]
+			);
+		},
+
+		getBlueprintsByNameAndAuthor:function(authname,bpname,callback){
+
+			callback(
+				mockdata[authname].find(function(e){return e.name===bpname})
+			);
+		}
+	}
+
+})();
+
+/*
+Example of use:
+var fun=function(list){
+	console.info(list);
+}
+
+apimock.getBlueprintsByAuthor("johnconnor",fun);
+apimock.getBlueprintsByNameAndAuthor("johnconnor","house",fun);*/
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,13 @@
+var apimock = apimock;
+
+
+var app = (function (){
+
+
+    return{
+        publicMethod: function test(){
+        alert("Prueba boton");
+        }
+    }
+
+})();
