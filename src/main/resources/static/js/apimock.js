0fac6f89cd48066f05a09c8b65a374e2df9bf32c
JULIAN <cisfjulian@gmail.com>
2023-03-15 14:40:34 -0500

    lab terminado

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -8,8 +8,10 @@ apimock=(function(){
 	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
 	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
 	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
-    mockdata["Test"] = [{author: "Test", "points":[{"x":137,"y":82},{"x":123,"y":161}], "name": "house4"},
-            	{author: "Test", "points":[{"x":75,"y":12},{"x":210,"y":167}], "name": "gear4"}];
+    mockdata["Munin"] = [{author: "Munin", "points":[{"x":137,"y":82},{"x":123,"y":161}], "name": "Lailiada"},
+     {author: "Munin", "points":[{"x":75,"y":12},{"x":210,"y":167}], "name": "Dacrowd"}];
+    mockdata["Chacon"] = [{author: "Chacon", "points":[{"x":85,"y":82},{"x":25,"y":189}], "name": "Leprision"},
+     {author: "Chacon", "points":[{"x":45,"y":32},{"x":36,"y":28}], "name": "Cathedral"}];
 
 	return {
 		getBlueprintsByAuthor:function(authname,callback){
